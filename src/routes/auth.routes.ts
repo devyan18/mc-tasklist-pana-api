748d2745e51692a9fd7a061d02614b54ba5b637c
devyan18 <epache17@gmail.com>
2024-10-05 06:14:25 -0300

    config of cookies

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -28,7 +28,7 @@ authRouter.post(
         .cookie(config.accessCookieName, response.accessToken, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'strict',
+          sameSite: 'none',
           secure: process.env.NODE_ENV === 'production',
         })
         .json(response);
@@ -55,7 +55,7 @@ authRouter.post(
         .cookie(config.accessCookieName, response.accessToken, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: 'strict',
+          sameSite: 'none',
           secure: process.env.NODE_ENV === 'production',
         })
         .json(response);
